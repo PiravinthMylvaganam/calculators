26171a0b4716912ab15211324cd792de5bab0ec4
PiravinthMylvaganam <135099757+PiravinthMylvaganam@users.noreply.github.com>
2024-07-21 15:39:17 +0200

    Add files via upload

---
+++ Taschenrechner in verschiedene Sprachen/PowerShell ISE/Taschenrechner.ps1
@@ -0,0 +1,50 @@
+﻿$wiederholen = "Ja"
+
+Write-Host "Willkommen bei meinem Taschenrechner in PowerShell ISE"
+
+While($wiederholen -eq "Ja")
+{
+    [int]$ersteNummer = Read-Host "Geben Sie die erste Zahl ein"
+    [int]$zweiteNummer = Read-Host "Geben Sie die zweite Zahl ein"
+
+    $operationszeichen = Read-Host "Welche Operationszeichen möchten Sie durchführen? (+, -, *, /, %)?"
+
+    Switch($operationszeichen)
+    {
+        +
+        {
+            Write-Host("Ergebnis der von Ihnen gewählten Operationszeichen ($operationszeichen) is: ")
+            $ersteNummer + $zweiteNummer
+        }
+
+        -
+        {
+            Write-Host("Ergebnis der von Ihnen gewählten Operationszeichen ($operationszeichen) is: ")
+            $ersteNummer - $zweiteNummer
+        }
+
+        *
+        {
+            Write-Host("Ergebnis der von Ihnen gewählten Operationszeichen ($operationszeichen) is: ")
+            $ersteNummer * $zweiteNummer
+        }
+
+        /
+        {
+            Write-Host("Ergebnis der von Ihnen gewählten Operationszeichen ($operationszeichen) is: ")
+            $ersteNummer / $zweiteNummer
+        }
+
+        %
+        {
+            Write-Host("Ergebnis der von Ihnen gewählten Operationszeichen ($operationszeichen) is: ")
+            $ersteNummer % $zweiteNummer
+        }
+
+        default
+        {
+            Write-Host "Bitte wählen Sie eines der verfügbaren Operationszeichen"
+        }
+    }
+    $wiederholen = Read-Host "möchten Sie eine weitere Berechnung durchführen (Ja / Nein)?"
+}
